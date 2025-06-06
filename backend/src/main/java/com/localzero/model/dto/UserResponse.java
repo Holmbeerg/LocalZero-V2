73acaa71ee4740e2037991f301d4579f87b4fa77
Holmberg <48940210+Holmbeerg@users.noreply.github.com>
2025-06-06 15:56:22 +0200

    rename userId to id in UserResponse DTO

--- backend/src/main/java/com/localzero/model/dto/UserResponse.java
+++ backend/src/main/java/com/localzero/model/dto/UserResponse.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record UserResponse(
-        Long userId,
+        Long id,
         String email,
         String name,
         String location,
