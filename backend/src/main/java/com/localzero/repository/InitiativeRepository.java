2760af9ea246fe878dda9a539bbcb2676e0bcabe
Holmberg <48940210+Holmbeerg@users.noreply.github.com>
2025-06-17 14:00:42 +0200

    refactor(backend): use EntityGraph in InitiativeRepository to avoid the N+1 query problem

--- backend/src/main/java/com/localzero/repository/InitiativeRepository.java
+++ backend/src/main/java/com/localzero/repository/InitiativeRepository.java
@@ -3,6 +3,7 @@ package com.localzero.repository;
 import com.localzero.model.Initiative;
 import com.localzero.model.User;
 import com.localzero.model.enums.Neighborhood;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface InitiativeRepository extends JpaRepository<Initiative, Long> {
+    @EntityGraph(attributePaths = {"creator", "participants"})
     @Query("""
     SELECT DISTINCT i FROM Initiative i
     LEFT JOIN InitiativeMember im ON i.id = im.initiative.id
@@ -22,6 +24,7 @@ public interface InitiativeRepository extends JpaRepository<Initiative, Long> {
     List<Initiative> findAllAccessibleByUser(@Param("user") User user,
                                              @Param("userNeighborhood") Neighborhood userNeighborhood);
 
+    @EntityGraph(attributePaths = {"creator", "participants"})
     @Query("""
             SELECT DISTINCT i FROM Initiative i
             LEFT JOIN InitiativeMember im ON i.id = im.initiative.id
