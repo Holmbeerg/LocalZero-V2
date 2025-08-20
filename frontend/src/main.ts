8573ec9bf0abf56eabc4a8675cf0f2a964917cdc
anotona <129624524+notonanotonanoton@users.noreply.github.com>
2025-08-21 00:21:29 +0200

    fix clickoutside syntax and comment the code out
    clickOutside.ts not working

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,13 +3,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
 import './style.css'
-import clickOutside from '@/directives/clickOutside.ts'
+//import clickOutside from './directives/clickOutside.ts'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
-app.directive('click-outside', clickOutside);
+
+//clickOutside.install(app)
 
 app.mount('#app')
+
+
+
